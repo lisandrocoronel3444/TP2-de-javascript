197400eca2ca9ce86738650d3afb3bcc3ab3a61e
Lisandro Coronel <lisandrojulian2004@gmail.com>
2023-03-13 00:17:32 -0300

    Realice el ejercicio numero 1

---
+++ ejercicio1.js
@@ -0,0 +1,9 @@
+let edad = prompt("introduce tu edad")
+if (Number(edad)==edad){
+    if(edad > 18){
+        document.write("Puedes conducir")
+    }
+}
+else{
+    alert("introduce un numero valido")
+}
